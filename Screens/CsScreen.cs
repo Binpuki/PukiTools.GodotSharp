6fb601e572ce34895330ae19e6456f3762e116ba
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-03-03 18:42:36 -0600

    Add screens and manager

---
+++ Screens/CsScreen.cs
@@ -0,0 +1,23 @@
+namespace PukiTools.GodotSharp.Screens;
+
+/// <summary>
+/// A main screen class for C# classes.
+/// </summary>
+[GlobalClass] public abstract partial class CsScreen : Node
+{
+    /// <summary>
+    /// Resources that will be loaded upon entering a loading screen.
+    /// </summary>
+    [Export] public ResourceLoadList ResourcesToLoad = new();
+    
+    /// <summary>
+    /// Triggers right after the scene is loaded to add resources to load.
+    /// </summary>
+    public abstract void ReadyPreload();
+
+    /// <summary>
+    /// Triggers upon loading a resource specified in <see cref="ResourcesToLoad"/>.
+    /// </summary>
+    /// <param name="path">The resource loaded.</param>
+    public abstract void OnPreload(string path);
+}
