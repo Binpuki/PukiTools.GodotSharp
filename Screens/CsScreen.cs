6077ae384d73c0aac658acedb7a61cf97daca13f
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-03-23 03:28:51 -0500

    If not preloaded, start preloading screens

--- Screens/CsScreen.cs
+++ Screens/CsScreen.cs
@@ -13,11 +13,28 @@ namespace PukiTools.GodotSharp.Screens;
     /// <summary>
     /// Triggers right after the scene is loaded to add resources to load.
     /// </summary>
-    public abstract void ReadyPreload();
+    public virtual void ReadyPreload() => _preloaded = true;
 
     /// <summary>
     /// Triggers upon loading a resource specified in <see cref="ResourcesToLoad"/>.
     /// </summary>
     /// <param name="path">The resource loaded.</param>
     public abstract void OnPreload(string path);
+    
+    private bool _preloaded = false;
+
+    /// <inheritdoc />
+    public override void _Ready()
+    {
+        if (_preloaded)
+            return;
+        
+        ScreenManager.SwitchScreen(GetSceneFilePath(), "default");
+    }
+    
+    /// <summary>
+    /// Whether this screen was preloaded first.
+    /// </summary>
+    /// <returns>True or false</returns>
+    protected bool IsPreloaded() => _preloaded;
 }
--- Screens/GDScreen.gd
+++ Screens/GDScreen.gd
@@ -1,13 +1,22 @@
-class_name GDScreen
-
-extends Node
+class_name GDScreen extends Node
 
 ## A main screen class for GDScript classes.
 
 @export var resources_to_load : ResourceLoadList = ResourceLoadList.new() ## Resources that will be loaded upon entering a loading screen.
 
 func ready_preload() -> void: ## Triggers right after the scene is loaded to add resources to load.
-	pass
+	_preloaded = true
 
 func on_resource_loaded(_path : String) -> void: ## Triggers upon loading a resource specified in [member resources_to_load]
 	pass
+
+var _preloaded : bool = false
+
+func _ready() -> void:
+	if _preloaded:
+		return
+	
+	ScreenManager.SwitchScreen(scene_file_path, "default")
+
+func is_preloaded() -> bool: ## Whether this screen was preloaded first.
+	return _preloaded
